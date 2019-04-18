939acb1e10d2f7f4129800690f9f907890130baa
mrmattkennedy <kennemat@mail.gvsu.edu>
2019-04-17 21:36:33 -0400

    Added sample2 edits

--- Lab11/sample2.c
+++ Lab11/sample2.c
@@ -6,13 +6,16 @@
 
 int main() 
 { 
+	struct stat statBuf;
 	DIR *dirPtr; 
 	struct dirent *entryPtr; 
 
 	dirPtr = opendir (".");
-
-	while ((entryPtr = readdir (dirPtr))) 
-		printf ("%-20s\n", entryPtr->d_name);
+	printf("%-20sSize (in bytes)\n", "File name"); 
+	while ((entryPtr = readdir (dirPtr))) {
+		stat(entryPtr->d_name, &statBuf);
+		printf ("%-20s%ld\n", entryPtr->d_name, statBuf.st_size);
+	}
 	closedir (dirPtr); 
 	return 0; 
 }
